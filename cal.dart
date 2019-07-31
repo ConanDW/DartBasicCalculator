97fc9da6d0eb7f2eda9970aba90afe2eb6b73305
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2019-07-31 19:06:44 -0400

    Better format for code; default statement for case statement

--- cal.dart
+++ cal.dart
@@ -1,81 +1,91 @@
 import "dart:io";
-import "dart:math";
 
 void main() {
-stdout.write("Additon, Subtraction, Multiplication, Divison, or Exit: ");
-var first = stdin.readLineSync();
-switch (first) {
-  case "Additon":
-   addnums();
-   break;
-  case "Subtraction": 
-   subnums();
-   break;
-  case "Multiplication": 
-   timesnums();
-   break; 
-  case "Divison": 
-   dividenums();
-   break;
-  case "Exit":
-   exit(0);
-   break;
-  case "addition":
-   addnums();
-   break;
-  case "divison":
-   dividenums();
-   break;
-  case "multiplication":
-   timesnums();
-   break;
-  case "subtraction":
-   subnums();
-   break;        
+  stdout.write("Additon, Subtraction, Multiplication, Divison, or Exit: ");
+  var first = stdin.readLineSync();
+  switch (first) {
+    case "Additon": {
+      addnums();
+      break;
+    }
+    case "Subtraction": {
+      subnums();
+      break;
+    }
+    case "Multiplication": {
+      timesnums();
+      break; 
+    }
+    case "Divison": {
+      dividenums();
+      break;
+    }
+    case "Exit": {
+      exit(0);
+      break;
+    }
+    case "addition": {
+      addnums();
+      break;
+    }
+    case "divison": {
+      dividenums();
+      break;
+    }  
+    case "multiplication": {
+      timesnums();
+      break;
+    }
+    case "subtraction": {
+      subnums();
+      break;
+    }   
+    default: {
+      print("Submitted answer not know, please try again.")
+    }   
  }
 }
 
 void addnums() {
-stdout.write("First Number: ");
-var conf = stdin.readLineSync();
-var numO = int.tryParse(conf);
-stdout.write("Second Number: ");
-var confTwo = stdin.readLineSync();
-var numT = int.tryParse(confTwo);
-print("${numO} + ${numT} = ${numO + numT}");
-main();
+  stdout.write("First Number: ");
+  var conf = stdin.readLineSync();
+  var numO = int.tryParse(conf);
+  stdout.write("Second Number: ");
+  var confTwo = stdin.readLineSync();
+  var numT = int.tryParse(confTwo);
+  print("${numO} + ${numT} = ${numO + numT}");
+  main();
 }
 
 void subnums() {
-stdout.write("First Number: ");
-var conf = stdin.readLineSync();
-var numO = int.tryParse(conf);
-stdout.write("Second Number: ");
-var confTwo = stdin.readLineSync();
-var numT = int.tryParse(confTwo);
-print("${numO} - ${numT} = ${numO - numT}");
-main();
+  stdout.write("First Number: ");
+  var conf = stdin.readLineSync();
+  var numO = int.tryParse(conf);
+  stdout.write("Second Number: ");
+  var confTwo = stdin.readLineSync();
+  var numT = int.tryParse(confTwo);
+  print("${numO} - ${numT} = ${numO - numT}");
+  main();
 }
 
 void timesnums() {
-stdout.write("First Number: ");
-var conf = stdin.readLineSync();
-var numO = int.tryParse(conf);
-stdout.write("Second Number: ");
-var confTwo = stdin.readLineSync();
-var numT = int.tryParse(confTwo);
-print("${numO} x ${numT} = ${numO * numT}");
-main();
+  stdout.write("First Number: ");
+  var conf = stdin.readLineSync();
+  var numO = int.tryParse(conf);
+  stdout.write("Second Number: ");
+  var confTwo = stdin.readLineSync();
+  var numT = int.tryParse(confTwo);
+  print("${numO} x ${numT} = ${numO * numT}");
+  main();
 }
 
 void dividenums() {
-stdout.write("First Number: ");
-var conf = stdin.readLineSync();
-var numO = int.tryParse(conf);
-stdout.write("Second Number: ");
-var confTwo = stdin.readLineSync();
-var numT = int.tryParse(confTwo);
-print("${numO} / ${numT} = ${numO / numT}");
-main();
+  stdout.write("First Number: ");
+  var conf = stdin.readLineSync();
+  var numO = int.tryParse(conf);
+  stdout.write("Second Number: ");
+  var confTwo = stdin.readLineSync();
+  var numT = int.tryParse(confTwo);
+  print("${numO} / ${numT} = ${numO / numT}");
+  main();
 }
-
